996de807210fa266608df173e65197b0b031debd
marzieh abidi <67004859+marzieh-ab@users.noreply.github.com>
2024-01-05 11:36:31 +0330

    add route chart

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,13 @@ import Home from "./Home";
 import Login from "./Login";
 import Tag from "./Tag";
 import Register from "./Register";
+import Chart from "./Chart";
 
 function App() {
   return (
     <>
       <Routes>
+      <Route path="chart" element={<Chart />} />
         <Route path="tag" element={<Tag />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
